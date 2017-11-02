73be0d1159507aa96af7ebdd6d00758dfa3c1e87
Jacek Kopecky <jacek@jacek.cz>
2017-11-02 00:02:21 +0000

    add basic put and list api

--- api/index.js
+++ api/index.js
@@ -4,8 +4,17 @@ const express = require('express');
 
 const api = express.Router();
 
+api.get('/', (req, res) => {
+  res.json(['a', 'b', 'c']);
+});
+
 api.get('/:id(\\w+)', (req, res) => {
   res.send(`hello ${req.params.id}`);
 });
+
+api.put('/:id(\\w+)', (req, res) => {
+  res.send(`saved ${req.params.id}`);
+});
+
 
 module.exports = api;
