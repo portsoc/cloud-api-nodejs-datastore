3275d05345de6883e0b2f96fe3741b38a4592c16
Jacek Kopecky <jacek@jacek.cz>
2017-11-02 11:45:29 +0000

    add a delay to inmemory db

--- api/db-inmemory.js
+++ api/db-inmemory.js
@@ -5,8 +5,26 @@ const data = {
   second: 'not here',
 };
 
-module.exports.list = () => Object.keys(data);
+let sleepTime = parseInt(process.env.SLEEP, 10);
+if (Number.isNaN(sleepTime)) sleepTime = 1000;
 
-module.exports.get = (id) => data[id];
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
-module.exports.put = (id, val) => { data[id] = val; };
+module.exports.list = async () => {
+  await sleep(sleepTime);
+  return Object.keys(data);
+};
+
+module.exports.get = async (id) => {
+  await sleep(sleepTime);
+  return data[id];
+};
+
+module.exports.put = async (id, val) => {
+  await sleep(sleepTime);
+  data[id] = val;
+};
